ec5907bba9042b4fadf734f368aecd38bcc1a91b
crdigital <clayton.metal2@hotmail.com>
2023-11-14 14:36:07 -0300

    Introdução ao EF/SQLite

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,42 +15,46 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[] 
-        {
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .net 5.",
-                Local = "Barra do Garças MT",
-                Lote = "1º Lote",
-                QuantidadePessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 12 e .net 6.",
-                Local = "Barra do Garças MT",
-                Lote = "2º Lote",
-                QuantidadePessoas = 550,
-                DataEvento = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            }
-        };
-       public EventoController()
+        // public IEnumerable<Evento> _evento = new Evento[] 
+        // {
+        //     new Evento(){
+        //         EventoId = 1,
+        //         Tema = "Angular 11 e .net 5.",
+        //         Local = "Barra do Garças MT",
+        //         Lote = "1º Lote",
+        //         QuantidadePessoas = 250,
+        //         DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+        //         ImagemURL = "foto.png"
+        //     },
+        //     new Evento(){
+        //         EventoId = 2,
+        //         Tema = "Angular 12 e .net 6.",
+        //         Local = "Barra do Garças MT",
+        //         Lote = "2º Lote",
+        //         QuantidadePessoas = 550,
+        //         DataEvento = DateTime.Now.AddDays(7).ToString("dd/MM/yyyy"),
+        //         ImagemURL = "foto.png"
+        //     }
+        // };
+        private readonly DataContext _context;
+        
+       public EventoController(DataContext context)
        {
+            _context = context;
         
        }            
         
         [HttpGet]
          public IEnumerable<Evento> Get()
         {
-            return _evento;
+            return _context.Eventos;
+            // return _evento;
         }  
 
         [HttpGet("{id}")]
-         public IEnumerable<Evento> GetById(int id)
+         public Evento GetById(int id)
         {
-            return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
